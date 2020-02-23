75c5aae1931a43d2e5c97b6332c0ecbab250e0b7
Rafel460 <rafaelmonte46@outlook.com>
2020-02-22 22:17:01 -0300

    beta

--- NetBeansProjects/CalcularEquacaoReta/src/calcularequacaoreta/Equacao.java
+++ NetBeansProjects/CalcularEquacaoReta/src/calcularequacaoreta/Equacao.java
@@ -12,20 +12,20 @@ package calcularequacaoreta;
 public class Equacao {
     
   
-    public String equacao;
-    public double membro_a;
-    public double membro_b;
-    public double membro_c;
+    private String equacao;
+    private String membro_a;
+    private String membro_b;
+    private String membro_c;
     
     public void calculo(double xa,double xb,double ya,double yb){
        double valor_x;
        double valor_y;
        double valor_c;
-       
+       //calculo
        valor_x = ya-yb;
        valor_y = xb-xa;
        valor_c = (xa*yb)-(xb*ya);
-       
+       //valores dos coeficientes de b e c
        if(valor_y < 0 && valor_c<0){
          System.out.println("Os valores da equação da reta são: "+valor_x+"x - "+valor_y+"y - "+valor_c+" = 0");
        }else if(valor_y<0 && valor_c>0){
@@ -36,6 +36,28 @@ public class Equacao {
          System.out.println("Os valores da equação da reta são: "+valor_x+"x + "+valor_y+"y + "+valor_c+" = 0");
        }
         
-        
+  
 }
+    //metodos set
+      public void setMembro_a(double valor_x){
+       this.membro_a = new String(""+valor_x+"x");
+      }
+      public void setMembro_b(double valor_y){
+          this.membro_b = new String(""+valor_y+"y");
+      }
+      public void setMembro_c(double valor_c){
+          this.membro_c = new String(""+valor_c+"");
+      }
+      public void setEquacao(double valor_x, double valor_y, double valor_c){
+          if(valor_y < 0 && valor_c < 0){
+          this.equacao = new String(""+valor_x+"x -"+valor_y+"y -"+valor_c+" = 0");
+          }else if(valor_y<0 && valor_c>0){
+         this.equacao = new String(""+valor_x+"x - "+valor_y+"y + "+valor_c+" = 0");
+       }else if(valor_c<0 && valor_y>0){
+         this.equacao = new String(""+valor_x+"x + "+valor_y+"y - "+valor_c+" = 0");
+       }else{
+         this.equacao = new String(""+valor_x+"x + "+valor_y+"y + "+valor_c+" = 0");
+       }
+      }
+      
 }
