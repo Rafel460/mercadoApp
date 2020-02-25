63c28ecd4e3ccc04d89e2f2f0f3c2a4a70c57006
rafaelmonte46@outlook.com <rafaelmonte46@outlook.com>
2020-02-25 15:15:25 -0300

    versao com email e recuperacao de senha

--- Área de trabalho/mercadoApp/src/app/login/login.page.ts
+++ Área de trabalho/mercadoApp/src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import{AngularFireAuth} from 'angularfire2/auth';
 import{Usuario} from '../classes/usuario/usuario';
 import{ToastController, AlertController} from '@ionic/angular';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -9,11 +10,29 @@ import{ToastController, AlertController} from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
   usuario : Usuario = new Usuario();
-  constructor(private autenticacao : AngularFireAuth, private toast : ToastController, private alerta : AlertController) { }
-
+  constructor(private autenticacao : AngularFireAuth, private toast : ToastController, private alerta : AlertController, private router: Router) { }
+  cont : number = 0;
   ngOnInit() {
   }
-  async loginErro(){
+  async pedidoRedefinicao(email){
+    const alerte = await this.alerta.create({
+      header : 'Oops',
+      message: 'Parece que você errou sua senha várias vezes, deseja mandar um pedido de redefinição de senha no seu e-mail?',
+      buttons: [{text : 'Não'},{
+        text : 'Sim',
+        handler : ()=>{
+          this.autenticacao.auth.sendPasswordResetEmail(email).catch((erro) => {console.log('Deu ruim')})
+        }
+      }]
+    })
+    await alerte.present();
+  }
+  async loginErro(email){
+    this.cont = this.cont+1;
+    console.log(this.cont);
+    if(this.cont == 3){
+      this.pedidoRedefinicao(email)
+    }
     const error = await this.toast.create({
       message : 'Email ou senha incorretos, por favor, tente novamente',
       buttons : ['Ok'],
@@ -23,7 +42,7 @@ export class LoginPage implements OnInit {
   }
   login(email, senha){
     this.autenticacao.auth.signInWithEmailAndPassword(email, senha).then(
-      () => {alert('Deu certo, só não criei a tela ainda kkk');}).catch((erro) => {this.loginErro()})
+      () => {alert('Deu certo, só não criei a tela ainda kkk');}).catch((erro) => {this.loginErro(email)})
     
   }
 
