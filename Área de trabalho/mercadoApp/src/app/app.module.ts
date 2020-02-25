ae957e033c639e728b4e85f115bb8e014481f48a
rafaelmonte46@outlook.com <rafaelmonte46@outlook.com>
2020-02-24 21:36:30 -0300

    versao 24/02

--- Área de trabalho/mercadoApp/src/app/app.module.ts
+++ Área de trabalho/mercadoApp/src/app/app.module.ts
@@ -8,13 +8,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireDatabaseModule, FormsModule, AngularFireModule.initializeApp({
+    apiKey: "AIzaSyDHSNPr0Zjw4CsF1UaG6WFs-1dHayX3V40",
+    authDomain: "projetomercado-67fee.firebaseapp.com",
+    databaseURL: "https://projetomercado-67fee.firebaseio.com",
+    projectId: "projetomercado-67fee",
+    storageBucket: "projetomercado-67fee.appspot.com",
+    messagingSenderId: "705626057614",
+    appId: "1:705626057614:web:34dca11a6f4fe9f88692df",
+    measurementId: "G-9WVHWFK403"
+
+  })],
   providers: [
     StatusBar,
+    AngularFireAuth,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
